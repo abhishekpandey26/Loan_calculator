02f2496804db1254d7c02c11d6c1505e0b54587c
Abhishek <abhishekpandeybhu@gmail.com>
2025-05-06 16:18:28 +0530

    create components

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+const ErrorPage = () => (
+  <Container>
+    <Typography variant="h4" gutterBottom>Error</Typography>
+    <Typography>Sorry, something went wrong.</Typography>
+  </Container>
+);
+
+export default ErrorPage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button, Switch } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { useGlobalContext } from '../context/GlobalContext.jsx';
+
+const Navbar = () => {
+  const { darkMode, setDarkMode } = useGlobalContext();
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>Loan App</Typography>
+        <Button color="inherit" component={Link} to="/">Home</Button>
+        <Button color="inherit" component={Link} to="/about">About</Button>
+        <Button color="inherit" component={Link} to="/exchange">Exchange</Button>
+        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} color="default" />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
