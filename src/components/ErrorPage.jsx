987895617dda372b3a6498f1764e5a759df5b4d5
Abhishek <abhishekpandeybhu@gmail.com>
2025-05-06 22:07:17 +0530

    final changes

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { Container, Typography } from '@mui/material';
+import React from "react";
+import { Container, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-const ErrorPage = () => (
-  <Container>
-    <Typography variant="h4" gutterBottom>Error</Typography>
-    <Typography>Sorry, something went wrong.</Typography>
-  </Container>
-);
+const ErrorPage = () => {
+  const navigate = useNavigate();
 
+  const handleRedirect = () => {
+    navigate("/");
+  };
+
+  return (
+    <Container>
+      <Typography variant="h4" gutterBottom>
+        Something went wrong in the application.
+      </Typography>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleRedirect}
+        style={{ marginTop: "16px" }}
+      >
+        Go to Home Page
+      </Button>
+    </Container>
+  );
+};
 export default ErrorPage;
